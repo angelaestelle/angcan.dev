4e7550473b61986a636e67ef9e37871bd357fc2c
Angela <angelaestmartinez@gmail.com>
2020-12-14 19:07:37 -0500

    updated sandbox lewks

--- js/main.js
+++
@@ -1,7 +0,0 @@
-function changeCursor() {
-    document.body.style.cursor = document.getElementById('cursorList').options[document.getElementById('example-cursor').selectedIndex].value;
-}
-
-function restoreCursor() {
-    document.body.style.cursor = 'default';
-}
