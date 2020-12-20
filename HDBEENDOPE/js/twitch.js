ed6569b7cd909608ac48e901929f5abfe7773252
Angela <angelaestmartinez@gmail.com>
2020-12-19 19:11:47 -0500

    background and music player

---
+++ HDBEENDOPE/js/twitch.js
@@ -0,0 +1,22 @@
+/* TWITCH API*/
+/* TWITCH API*/
+/* TWITCH API*/
+
+//Create a Twitch.Embed object that 
+//will render within the "twitch-embed" root element.
+
+const embed = document.getElementById('twitch-embed')
+
+new Twitch.Embed("twitch-embed", {
+  width: 854,
+  height: 480,
+  channel: "lecheflana",
+  allowfullscreen: true,
+  autoplay: true,
+  layout: 'video-with-chat',
+});
+
+embed.addEventListener(Twitch.Embed.VIDEO_READY, () => {
+  var player = embed.getPlayer();
+  player.play();
+});
